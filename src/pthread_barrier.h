074247f5954cb8c02e3b61ebe258c7c2c0fb3c23
Dave Reed <dave256@mac.com>
2019-01-26 07:15:01 -0500

    remove restrict that generated compile issues

--- src/pthread_barrier.h
+++ src/pthread_barrier.h
@@ -66,10 +66,10 @@ int pthread_barrierattr_getpshared(const pthread_barrierattr_t *restrict attr,
 int pthread_barrierattr_setpshared(pthread_barrierattr_t *attr,
 				   int pshared);
 
-int pthread_barrier_init(pthread_barrier_t *restrict barrier,
-			 const pthread_barrierattr_t *restrict attr,
-			 unsigned int count);
-int pthread_barrier_destroy(pthread_barrier_t *barrier);
+    int pthread_barrier_init(pthread_barrier_t *barrier,
+                             const pthread_barrierattr_t *attr,
+                             unsigned int count);
+    int pthread_barrier_destroy(pthread_barrier_t *barrier);
 
 int pthread_barrier_wait(pthread_barrier_t *barrier);
 
